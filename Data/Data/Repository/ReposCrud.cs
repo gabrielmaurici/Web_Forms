6cb61e092f6a7ed4de21d21f7f95f5742accdcab
Gabriel Maurici <gabriel.maurici@hotmail.com>
2021-07-24 14:00:53 -0300

    Add classe Crud com delegate

---
+++ Data/Data/Repository/ReposCrud.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Models;
+
+namespace Data.Repository
+{
+    public delegate void Operacao();
+    class ReposCrud
+    {
+        protected List<BaseModel> lista = new List<BaseModel>();
+        protected Operacao opCadastrar;
+        protected Operacao opListar;
+
+        public void Cadastrar()
+        {
+            opCadastrar();
+        }
+        puvlic void Listar()
+        {
+            opListar();
+        }
+    }
+}
--- Data/Data/Repository/RepositoryCategoria.cs
+++ Data/Data/Repository/RepositoryCategoria.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data.Models;
 
 namespace Data.Repository
 {
     class RepositoryCategoria
     {
-
+        public RepositoryCategoria()
+        {
+            
+        }
     }
 }
