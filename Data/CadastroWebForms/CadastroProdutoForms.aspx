5e221a79f4980534fccd9800300e0efd1b3e99e2
Gabriel Maurici <gabriel.maurici@hotmail.com>
2021-07-25 12:08:40 -0300

    Add Web Forms e web Cadastro Produto

---
+++ Data/CadastroWebForms/CadastroProdutoForms.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CadastroProdutoForms.aspx.cs" Inherits="CadastroWebForms.CadastroProdutoForms" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 484px">
+            <asp:Label ID="lbNome" runat="server" Text="Nome"></asp:Label>
+            <asp:TextBox ID="tbNome" runat="server"></asp:TextBox>
+            <br/>
+            <br />
+             <asp:Label ID="Label1" runat="server" Text="Nome"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Data/CadastroWebForms/CadastroProdutoForms.aspx.designer.cs
@@ -0,0 +1,62 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace CadastroWebForms
+{
+
+
+    public partial class CadastroProdutoForms
+    {
+
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controle lbNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbNome;
+
+        /// <summary>
+        /// Controle tbNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbNome;
+
+        /// <summary>
+        /// Controle Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Controle TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+    }
+}
